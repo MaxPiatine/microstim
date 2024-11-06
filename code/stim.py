1d2b1138fd09d74c21c93bddee8c5ba116dfaccb
MaxPiatine <max.piatine@hotmail.com>
2024-11-05 21:33:26 -0500

    init conditions

--- code/stim.py
+++ code/stim.py
@@ -8,7 +8,7 @@ For excitatory potential V_e
 """
 
 R = 7 #resistance (change) KOhms
-I = 45 #initial intensity (change) muA
+I = 200 #initial intensity (change) muA
 ALPHA = 4 #micron
 N = 10
 DT = 0.01
@@ -18,10 +18,10 @@ E_THRESHOLD, I_THRESHOLD = 20, 20 #mV
  
 # look up the weights
 weights = {
-    "e->e": 1,
-    "i->e": 1,
-    "e->i": 1,
-    "i->i": 1,
+    "e->e": 400,
+    "i->e": 150,
+    "e->i": 150,
+    "i->i": 50,
 }
 
 sigma = {
@@ -37,13 +37,19 @@ def KernelConvolution(x, rho, weight, sigma, threshold=20):
 
 
 i_RANGE = arange(0, N)
-X_RANGE = arange(-1000, 1000)
-rho_e, rho_i = zeros(N), zeros(N)
+X_RANGE = arange(0, 1000)
 
 v_e, v_i = zeros((len(i_RANGE), len(X_RANGE))), zeros((len(i_RANGE), len(X_RANGE)))
+rho_e, rho_i = zeros(N), zeros(N)
 
-# init conditions
+"""
+Initial condition is assuming that we stimulate a population of 
+excitatory cells. where the excitatory potential follows monopole
+diapole, and the activation radius is dependent on the intensity
+"""
 rho_e[0] = R*I/E_THRESHOLD - ALPHA
+v_e[0][:] = R*I/(X_RANGE + ALPHA)
+
 
 """
 kernal convolution array
@@ -51,8 +57,7 @@ kernal convolution array
 ee_erf, ie_erf = zeros((len(i_RANGE), len(X_RANGE))), zeros((len(i_RANGE), len(X_RANGE)))
 
 for i in range(len(i_RANGE)-1):
-    print("STEP %i"%i)
-
+    
     ee_erf[i][:] = KernelConvolution(X_RANGE, rho_e[i], weights["e->e"], sigma["ee"]) 
     ie_erf[i][:] = KernelConvolution(X_RANGE, rho_i[i], weights["i->e"], sigma["ie"])
     v_e[i+1][:] += v_e[i][:] + DT * (-v_e[i][:] + ee_erf[i][:] - ie_erf[i][:])
@@ -62,21 +67,21 @@ for i in range(len(i_RANGE)-1):
         )
 
     for index, e_potential in enumerate(v_e[i+1][:]):
-        if e_potential >= E_THRESHOLD:
-            rho_e[i] += 1
+        if e_potential < E_THRESHOLD:
+            rho_e[i] = X_RANGE[index]
+            break
 
     for index, i_potential in enumerate(v_i[i+1][:]):
-        if i_potential >= I_THRESHOLD:
-            rho_i[i] += 1
-
-    rho_e[i] *= E_THRESHOLD
-    rho_i[i] *= I_THRESHOLD
+        if i_potential < I_THRESHOLD:
+            rho_i[i] = X_RANGE[index]
+            break
+            
+    print("At Step %i: excitatory activation radius: %i inhibitory activation radius: %i"%(i, rho_e[i], rho_i[i]))
 
 
 figure, ax = plt.subplots(1, 2)
 
 for i in i_RANGE:
-    print(rho_e[i])
     ax[0].plot(X_RANGE, v_e[i], label="ve[%i]"%i)
     ax[1].plot(X_RANGE, v_i[i], label="vi[%i]"%i)
 
